073ce1a5c6e014bc96040441bab4a01ce224ee82
novusweb3 <bleachssp501@gmail.com>
2023-02-28 23:37:50 +0000

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App(props) {
+  const onTellJoke = () => {
+    fetch("https://icanhazdadjoke.com/", {
+      method: "GET",
+      headers: {
+        Accept: "application/json"
+      }
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  };
+  return <button onClick={onTellJoke}>Tell me a joke</button>;
+}
+
+const rootElement = document.getElementById('root');
+createRoot(rootElement).render(<App />);
